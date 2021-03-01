c929df5dd802e817844f61883fd29a15011c0b45
Emani Butler <77745621+HelloLovelyWorld@users.noreply.github.com>
2021-02-28 16:43:38 -0800

    Program that shows how to swap two numbers.

---
+++ SwappingNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice_CodingProblems
+{
+    class SwappingNumbers
+    {
+        //Write a C# Sharp program to swap two numbers.
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+
+            Console.WriteLine("Enter first number.");
+            num1 = Int32.Parse(Console.ReadLine()); // 6
+            Console.WriteLine("Enter second number.");
+            num2 = Int32.Parse(Console.ReadLine()); // 5
+
+            temp = num1; // 6
+            num1 = num2; // 5
+            num2 = temp; // 6
+
+            Console.WriteLine("After swapping numbers:");
+            Console.WriteLine("First Number: " + num1);
+            Console.WriteLine("Second number: " + num2);
+            
+        }
+    }
+}
