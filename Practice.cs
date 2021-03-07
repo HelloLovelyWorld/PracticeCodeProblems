de3a730bd794092065b115dea34ffd251abad981
Emani Butler <77745621+HelloLovelyWorld@users.noreply.github.com>
2021-03-06 19:08:49 -0800

    How to print out a reversed string.

---
+++ Practice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice_CodingProblems
+{
+    class Practice
+    {
+        // How to print out a reversed string.
+        static void Main(string[] args)
+        {
+            string myString = "Hello World";
+            var characters = myString.ToCharArray();
+            var length = characters.Length;
+
+            for (var i = length - 1; i >= 0; i--)
+            {
+                Console.Write(characters[i]);
+            }
+        }
+    }
+}
