284cce36e00a7dd8f806a180b1cb5efd6ff94ab6
Emani Butler <77745621+HelloLovelyWorld@users.noreply.github.com>
2021-03-07 10:17:38 -0800

    Write a method that finds the length of a string.

---
+++ LengthofString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice_CodingProblems
+{
+    class Practice
+    {
+        //Write a methed that finds the length of a string.
+        static void Main(string[] args)
+        {
+            string mySentence = "I love technology.";
+            var sentenceLength = mySentence.Length;
+
+            Console.WriteLine($"My sentence is: {mySentence}");
+            Console.WriteLine($"It has {sentenceLength} characters in it.");
+        }
+    }
+}
