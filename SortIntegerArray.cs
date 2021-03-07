b492ac5a1874aee910c2d460cd3a1a2a401c18c6
Emani Butler <77745621+HelloLovelyWorld@users.noreply.github.com>
2021-03-07 10:35:21 -0800

    Create a program that will sort an integer array.

---
+++ SortIntegerArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice_CodingProblems
+{
+    class Practice
+    {
+        //C#: Create a program that will sort an integer array. 
+        static void Main(string[] args)
+        {
+            int[] nums = { 8, 4, 9, 2, 6 };
+            Array.Sort(nums);
+
+            foreach (int number in nums)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
