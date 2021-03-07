2b873e546c0a033c820f0128d7ff74b6e08021bd
Emani Butler <77745621+HelloLovelyWorld@users.noreply.github.com>
2021-03-07 10:10:44 -0800

    Write a method that displays the longest word in a sentence.

---
+++ LongestWord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_CodingProblems
+{
+    class LongestWord
+    {
+        // Write a method that displays the longest word in a sentence.
+        static void Main(string[] args)
+        {
+            string mySentence = "I love technology.";
+            var largestWord = " ";
+            string[] array = mySentence.Split(' ');
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length >largestWord.Length)
+                {
+                    largestWord = array[i];
+                }
+            }
+            Console.WriteLine($"The sentence is: \"{mySentence}\"");
+            Console.WriteLine($"The largest word in this sentence is: {largestWord}");
+        }
+    }
+}
